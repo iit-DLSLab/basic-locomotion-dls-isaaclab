4608c6bb64fe958bf4065097f7917dd1c34bf980
giulioturrisi <giulioturrisi@hotmail.it>
2026-06-10 14:27:20 +0200

    set default policy

--- deploy/config.py
+++ deploy/config.py
@@ -21,10 +21,10 @@ elif(robot == "go2"):
     Kp_walking = 20.0
     Kd_walking = 2.0 #1.5
 
-    Kp_stand_up_and_down = 25.
-    Kd_stand_up_and_down = 2.
+    Kp_stand_up_and_down = 50.
+    Kd_stand_up_and_down = 5.
 
-    policy_folder_path = dir_path + "/../tested_policies/" + robot + "/symm"
+    policy_folder_path = dir_path + "/../tested_policies/" + robot + "/concurrent_symm"
 
 elif(robot == "b2"):
     Kp_walking = 100.
