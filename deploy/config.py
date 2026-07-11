dddf913f23afc752f45d46af581f7558cab8fb51
giulioturrisi <giulioturrisi@hotmail.it>
2026-07-11 07:45:52 +0200

    renaming policy

--- deploy/config.py
+++ deploy/config.py
@@ -24,7 +24,7 @@ elif(robot == "go2"):
     Kp_stand_up_and_down = 50.
     Kd_stand_up_and_down = 5.
 
-    policy_folder_path = dir_path + "/../tested_policies/" + robot + "/concurrent_symm"
+    policy_folder_path = dir_path + "/../tested_policies/" + robot + "/concurrent_nongaited"
 
 elif(robot == "b2"):
     Kp_walking = 100.
