92c6e971405c2adcb070ec4e4a0f220ea2e1a459
giulioturrisi <giulioturrisi@outlook.it>
2026-08-10 14:51:38 +0200

    increased go2 stepping height

--- source/basic_locomotion_isaaclab/basic_locomotion_isaaclab/tasks/locomotion/go2_env_cfg.py
+++ source/basic_locomotion_isaaclab/basic_locomotion_isaaclab/tasks/locomotion/go2_env_cfg.py
@@ -331,7 +331,7 @@ class Go2FlatEnvCfg(DirectRLEnvCfg):
     
     # Desired tracking variables
     desired_base_height = 0.30
-    desired_feet_height = 0.05
+    desired_feet_height = 0.08
 
 
     # Desired clip actions
