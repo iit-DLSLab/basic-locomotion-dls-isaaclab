de24f7985aaac8fc0ba2d876a7323664893408b7
giulioturrisi <giulioturrisi@hotmail.it>
2026-08-31 18:32:50 +0200

    using new control signal

--- deploy/run_simulator_ros2.py
+++ deploy/run_simulator_ros2.py
@@ -37,7 +37,7 @@ if os.environ.get("BASIC_LOCOMOTION_ROS2_SOURCED") != "1":
 
 import rclpy 
 from rclpy.node import Node 
-from dls2_interface.msg import BaseState, BlindState, Imu, TrajectoryGenerator
+from dls2_interface.msg import BaseState, BlindState, Imu, ControlSignal
 from unitree_go.msg import LowState, MotorState, IMUState
 
 import time
@@ -77,7 +77,7 @@ class SimulatorROS2(Node):
 
         self.publisher_low_state = self.create_publisher(LowState,"lowstate", 1)
 
-        self.subscriber_trajectory_generator = self.create_subscription(TrajectoryGenerator,"trajectory_generator", self.get_trajectory_generator_callback, 1)
+        self.subscriber_control_signal = self.create_subscription(ControlSignal,"control_signal", self.get_control_signal_callback, 1)
 
         self.timer = self.create_timer(1.0/SCHEDULER_FREQ, self.compute_simulator_step_callback)
 
@@ -111,7 +111,7 @@ class SimulatorROS2(Node):
         self.Kd = 0
 
 
-    def get_trajectory_generator_callback(self, msg):
+    def get_control_signal_callback(self, msg):
 
         joints_position = np.array(msg.joints_position)
 
